940004e399a5dd5e197984adfe9a3d0bd2ce9dd6
GilesGepulane <2024-2-00149@lpunetwork.edu.ph>
2024-11-07 23:38:58 +0800

    first ver

---
+++ collatz.py
@@ -0,0 +1,19 @@
+import time
+
+num = 2
+steps = 0
+
+if num > 0:
+    while num != 1:
+        steps +=1
+        if num%2 == 0:
+            num //=2
+            print(num)
+        else:
+             num = 3 * num + 1
+             print(num)
+    else:
+        num+=1
+else:
+    print("Error")
+print("Steps:",steps)
